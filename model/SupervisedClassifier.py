2bf8e2c2da56235d03f39dfc65cdd5adae00ec2e
dany9999 <daniele99abbagnale@gmail.com>
2025-09-20 11:04:26 +0200

    fix

--- model/SupervisedClassifier.py
+++ model/SupervisedClassifier.py
@@ -44,13 +44,13 @@ class BIOTClassifier(nn.Module):
             mask_ratio=0.0,
             **kwargs
         )
-        self.pooling = AttentionPooling(emb_size)
+        #self.pooling = AttentionPooling(emb_size)
         self.classifier = ClassificationHead(emb_size, n_classes)
 
     def forward(self, x, n_channel_offset=0):
         _, _, out_biot, _ = self.biot(x, n_channel_offset)  # out_biot: (B, Seq, D)
-        pooled = self.pooling(out_biot)                      # (B, D)
-        logits = self.classifier(pooled)                     # (B, 1)
+        #pooled = self.pooling(out_biot)                      # (B, D)
+        logits = self.classifier(out_biot)                     # (B, 1)
         return logits
 
 
