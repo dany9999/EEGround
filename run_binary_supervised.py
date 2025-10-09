1c7ab65040779dff2d6040474495d120f310280f
dany9999 <daniele99abbagnale@gmail.com>
2025-10-09 13:27:56 +0200

    fix

--- run_binary_supervised.py
+++ run_binary_supervised.py
@@ -188,21 +188,21 @@ def supervised(config):
         hop_length=100,
     )
 
-    # 🔹 Caricamento pesi pretrained se specificato
-    if config.get("pretrain_model_path", ""):
-        state = torch.load(config["pretrain_model_path"], map_location="cpu")
-        model_dict = model.biot.state_dict()
+    # # 🔹 Caricamento pesi pretrained se specificato
+    # if config.get("pretrain_model_path", ""):
+    #     state = torch.load(config["pretrain_model_path"], map_location="cpu")
+    #     model_dict = model.biot.state_dict()
 
-        # allinea i layer con la stessa shape
-        compatible_state = {k: v for k, v in state.items()
-                            if k in model_dict and v.shape == model_dict[k].shape}
-        missing = set(model_dict.keys()) - set(compatible_state.keys())
-        print(f"Carico {len(compatible_state)} layer dai pretrained, "
-              f"{len(missing)} inizializzati random.")
+    #     # allinea i layer con la stessa shape
+    #     compatible_state = {k: v for k, v in state.items()
+    #                         if k in model_dict and v.shape == model_dict[k].shape}
+    #     missing = set(model_dict.keys()) - set(compatible_state.keys())
+    #     print(f"Carico {len(compatible_state)} layer dai pretrained, "
+    #           f"{len(missing)} inizializzati random.")
 
-        # aggiorna i pesi
-        model_dict.update(compatible_state)
-        model.biot.load_state_dict(model_dict)
+    #     # aggiorna i pesi
+    #     model_dict.update(compatible_state)
+    #     model.biot.load_state_dict(model_dict)
 
 
     lightning_model = LitModel_finetune(config, model)
@@ -210,11 +210,11 @@ def supervised(config):
     version = f"CHB-MIT-{config["finetune_mode"]}"
     logger = TensorBoardLogger(save_dir="./", version=version, name="log")
 
-    early_stop_callback = EarlyStopping(monitor="val_bacc", patience=config["early_stopping_patience"], verbose=False, mode="max")
+    early_stop_callback = EarlyStopping(monitor="val_pr_auc", patience=config["early_stopping_patience"], verbose=False, mode="max")
 
 
     checkpoint_callback = ModelCheckpoint(
-    monitor="val_bacc",
+    monitor="val_pr_auc",
     mode="max",
     save_top_k=1,
     filename="best-model"
@@ -264,6 +264,7 @@ def objective(trial):
     config["lr"] = trial.suggest_loguniform("lr", 1e-6, 1e-4)
     config["focal_alpha"] = trial.suggest_uniform("focal_alpha", 0.2, 0.9)
     config["focal_gamma"] = trial.suggest_uniform("focal_gamma", 1.0, 5.0)
+    config["weight_decay"] = trial.suggest_loguniform("weight_decay", 1e-6, 1e-2)
     #config["threshold"] = trial.suggest_uniform("threshold", 0.1, 0.9)
 
     # Limita epoche per tuning veloce
@@ -271,7 +272,7 @@ def objective(trial):
 
     # Allena e ottieni risultati
     results = supervised(config)  # deve ritornare i risultati
-    return results["val_bacc"]  # metriche monitorate
+    return results["val_pr_auc"]  # metriche monitorate
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
