bc5f1eed3e48ad4aa25c97a787428b9da4b14b79
dany9999 <daniele99abbagnale@gmail.com>
2025-10-31 17:28:52 +0100

    fix

--- CHBMITLoader_4s.py
+++ CHBMITLoader_4s.py
@@ -154,9 +154,9 @@ def make_loader(patient_ids, dataset_path, gt_path, config, shuffle=True, balanc
 
 if __name__ == "__main__":
     # Split pazienti
-    train_patients = [f"chb{str(i).zfill(2)}" for i in range(1,5)]
-    val_patients   = [f"chb{str(i).zfill(2)}" for i in range(1, 5)]
-    test_patients  = [f"chb{str(i).zfill(2)}" for i in range(1, 5)]
+    train_patients = [f"chb{str(i).zfill(2)}" for i in range(1,10)]
+    val_patients   = [f"chb{str(i).zfill(2)}" for i in range(1, 10)]
+    test_patients  = [f"chb{str(i).zfill(2)}" for i in range(1, 10)]
 
     config = load_config("configs/finetuning.yml")
     dataset_path = config["dataset_path_4s"]
