9bd188f1590a5efcf6b09934edfa604fed9a9018
dany9999 <daniele99abbagnale@gmail.com>
2025-11-01 12:29:00 +0100

    fix

--- check_pesi.py
+++ check_pesi.py
@@ -1,17 +1,5 @@
 import torch
-from BIOT_vanilla.biot import BIOTClassifier
-
-ckpt = torch.load("BIOT_vanilla/pretrained_models/EEG-PREST-16-channels.ckpt", map_location="cpu")
-
-# Se Lightning
-if "state_dict" in ckpt:
-    ckpt = ckpt["state_dict"]
-
-print("\nEsempio di chiavi nel checkpoint:")
-for k in list(ckpt.keys())[:20]:
-    print(k)
-
-model =     model = BIOTClassifier( n_channels=18, n_fft=200,hop_length=100,)
-print("\nEsempio di chiavi nel modello:")
-for k in list(model.state_dict().keys())[:20]:
-    print(k)
+ckpt = torch.load("logs/pretrain/encoder_only_epoch_1.pt", map_location="cpu")
+print("Totale chiavi:", len(ckpt.keys()))
+for i, k in enumerate(list(ckpt.keys())[:30]):
+    print(f"{i+1:02d}. {k}")
