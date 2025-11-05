41bc4c9854335cf5418a90cefe0510412ffabb8b
dany9999 <daniele99abbagnale@gmail.com>
2025-11-05 10:51:01 +0100

    fix

--- run_binary_supervised.py
+++ run_binary_supervised.py
@@ -367,8 +367,8 @@ def supervised(config):
 
     lightning_model = LitModel_finetune(config, model)
 
-    #version = f"lr{config['lr']}-channels{config['n_channels']}-nfft{config['n_fft']}-hop{config['hop_length']}-4s-pretrained"
-    version = f"encLR{config['encoder_lr']:.1e}_headLR{config['head_lr']:.1e}"
+    version = f"lr{config['lr']}-channels{config['n_channels']}-nfft{config['n_fft']}-hop{config['hop_length']}-4s-pretrained"
+    #version = f"encLR{config['encoder_lr']:.1e}_headLR{config['head_lr']:.1e}"
     logger = TensorBoardLogger(save_dir="./", version=version, name=config["log_dir"])
 
     early_stop_callback = EarlyStopping(monitor="val_bacc", patience=config["early_stopping_patience"], verbose=False, mode="max")
